415fc83ea58c285e31dad2014116656ffbbd37b0
ElenaHrudavenka <kisseli@mail.ru>
2022-03-21 20:58:54 +0300

    Update Skills.js

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import style from './Skills.module.css'
 import styleContainer from  './../common/styles/Container.module.css'
+import Skill from "./skill/Skill";
 
-const Skills = () => {
+const Skills = (props) => {
+    const listSkills = props.skills.map(el => {
+        return (
+
+                <Skill key={el.id} icon = {el.icon} title={el.title} description={el.description}/>
+
+        )
+    })
     return (
-        <div className={style.skillsBlock}> 
+        <div className={style.skillsBlock}>
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>My Services</h2>
-                <div className={style.skills}></div>
+                <div className={style.skills}>
+                {listSkills}
+                </div>
             </div>
         </div>
     );
