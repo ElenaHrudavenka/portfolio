a2d123d98b52fcb8f7f20b655f2bf2f00ca242ab
ElenaHrudavenka <kisseli@mail.ru>
2022-03-22 14:36:05 +0300

    min-height for mobile version
    min-height for mobile version

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
         {id: v1(), icon: "", title: "JavaScript", description: "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
         {id: v1(), icon: "", title: "TypeScript", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
         {id: v1(), icon: "", title: "REACT", description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."},
-   /*     {id: v1(), icon: "", title: "CSS", description: "amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
-        {id: v1(), icon: "", title: "HTML", description: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},*/
+        {id: v1(), icon: "", title: "CSS", description: "amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
+        {id: v1(), icon: "", title: "HTML", description: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},
     ]
     return (
         <div className="App">
