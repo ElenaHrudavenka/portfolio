7d4f883ce85cc796369103face703df8ad6e4fce
ElenaHrudavenka <kisseli@mail.ru>
2022-03-29 13:01:26 +0300

    Create Project.js

---
+++ src/projects/project/Project.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './Project.module.css'
+
+const Project = (props) => {
+    return (
+        <div className={style.project}>
+            <div className={style.image}>
+                <a href={props.link} className={style.button}>Look</a>
+            </div>
+            <h3>{props.projectTitle}</h3>
+            <span className={style.description}>{props.description}</span>
+        </div>
+    );
+};
+
+export default Project;
