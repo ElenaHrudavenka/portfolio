418bf36f763a8d7641c370056bc1116c1a46b09d
ElenaHrudavenka <kisseli@mail.ru>
2022-05-19 16:54:28 +0300

    Update Nav.js

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -4,7 +4,8 @@ import style from './Nav.module.css'
 const Nav = () => {
     return (
         <div className={style.nav}>
-            <a href="">Main page</a>
+            {/*<a href="">Main page</a>*/}
+            <a href="">Home</a>
             <a href="">Skills</a>
             <a href="">Projects</a>
             <a href="">Contacts</a>
